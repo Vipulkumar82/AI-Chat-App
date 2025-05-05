33b3bad7ae34d32fd302ca5b61f27225e32fe800
vipulkumar82 <vipulkumar212003@gmail.com>
2025-05-06 00:06:31 +0530

    Implimented Ai Feature in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 import './index.css'
+import axios from 'axios'
 
 function App() {
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
+    async function generateAnswer(){
+      setAnswer("Loading...")
+      const response = await axios({
+        url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyB9DaUQSDjgUbRaNIfujV_2Indjunf2qqs",
+        method:"post",
+        data: {
+          contents: [
+            {parts: [{ text: question}]},
+          ],
+        },
+      });
+      setAnswer(response['data']['candidates'][0]['content'] ['parts'][0]['text'])
+    }
+
   return (
     <>
       <div id="app">
       <div className='main'>
-        <textarea placeholder='Enter the Question'></textarea>
-        <button>Submit</button>
+        <h1>ChatWith AI 🖤</h1>
+        <textarea placeholder='Enter the Question' value={question} onChange={(e)=> setQuestion(e.target.value)}></textarea>
+        <button onClick={generateAnswer}>Submit</button>
         <div className="output">
           <h3>Output:</h3>
         </div>
         <div className="output-Box">
-          Hey I'm The Output
+          <pre>{answer}</pre>
         </div>
       </div>
         <footer className='footer'>&copy;All Rights are reserved by @Vipul Thakur♠️</footer>
